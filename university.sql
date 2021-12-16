6c51c4c15433ed5a06c510b048d753b4485e5ce8
David Morales <5dmorales474@gmail.com>
2021-12-16 23:13:00 +0100

    Cleaning responses with Joins with info not needed

--- university.sql
+++ university.sql
@@ -11,23 +11,23 @@ SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'profesor' AND tel
 -- 5    Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7.
 SELECT * FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
 -- 6    Retorna un llistat dels professors juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom.
-SELECT persona.apellido1, persona.apellido2, persona.nombre, departamento.nombre FROM persona INNER JOIN profesor ON persona.id = profesor.id_profesor INNER JOIN departamento ON profesor.id_departamento = departamento.id;
+SELECT persona.apellido1, persona.apellido2, persona.nombre, departamento.nombre FROM persona INNER JOIN profesor ON persona.id = profesor.id_profesor INNER JOIN departamento ON profesor.id_departamento = departamento.id ORDER BY persona.apellido1, persona.apellido2, persona.nombre;
 -- 7    Retorna un llistat amb el nom de les assignatures, any d'inici i any de fi del curs escolar de l'alumne amb nif 26902806M.
 SELECT asignatura.nombre, curso_escolar.anyo_inicio, curso_escolar.anyo_fin FROM persona INNER JOIN alumno_se_matricula_asignatura ON persona.id = alumno_se_matricula_asignatura.id_alumno INNER JOIN asignatura ON asignatura.id = alumno_se_matricula_asignatura.id_asignatura INNER JOIN curso_escolar ON curso_escolar.id = asignatura.curso WHERE nif = '26902806M';
 -- 8    Retorna un llistat amb el nom de tots els departaments que tenen professors que imparteixen alguna assignatura en el Grau en Enginyeria Informàtica (Pla 2015).
-SELECT * FROM departamento INNER JOIN profesor ON departamento.id = profesor.id_departamento INNER JOIN asignatura ON profesor.id_profesor = asignatura.id_profesor INNER JOIN grado ON asignatura.id_grado = grado.id WHERE grado.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
+SELECT * FROM departamento WHERE id IN (SELECT departamento.id FROM departamento INNER JOIN profesor ON departamento.id = profesor.id_departamento INNER JOIN asignatura ON profesor.id_profesor = asignatura.id_profesor INNER JOIN grado ON asignatura.id_grado = grado.id WHERE grado.nombre = 'Grado en Ingeniería Informática (Plan 2015)');
 -- 9    Retorna un llistat amb tots els alumnes que s'han matriculat en alguna assignatura durant el curs escolar 2018/2019.
 SELECT * FROM persona WHERE id IN (SELECT DISTINCT(persona.id) FROM persona LEFT JOIN alumno_se_matricula_asignatura ON persona.id = alumno_se_matricula_asignatura.id_alumno LEFT JOIN curso_escolar ON alumno_se_matricula_asignatura.id_curso_escolar = curso_escolar.id WHERE curso_escolar.anyo_inicio = 2018 and curso_escolar.anyo_fin = 2019);
 -- 10   Retorna un llistat amb els noms de tots els professors i els departaments que tenen vinculats. El llistat també ha de mostrar aquells professors que no tenen cap departament associat. El llistat ha de retornar quatre columnes, nom del departament, primer cognom, segon cognom i nom del professor. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, cognoms i el nom.
 SELECT departamento.nombre, persona.apellido2, persona.apellido1,persona.nombre FROM persona RIGHT JOIN profesor ON persona.id = profesor.id_profesor LEFT JOIN departamento ON profesor.id_departamento = departamento.id ORDER BY departamento.nombre, persona.apellido2, persona.apellido1,persona.nombre;
 -- 11   Retorna un llistat amb els professors que no estan associats a un departament.
-SELECT * FROM persona RIGHT JOIN profesor ON persona.id = profesor.id_profesor WHERE profesor.id_departamento IS NULL;
+SELECT * FROM persona WHERE id IN (SELECT persona.id FROM persona RIGHT JOIN profesor ON persona.id = profesor.id_profesor WHERE profesor.id_departamento IS NULL);
 -- 12   Retorna un llistat amb els departaments que no tenen professors associats.
-SELECT * FROM departamento LEFT JOIN profesor ON profesor.id_departamento = departamento.id WHERE profesor.id_profesor IS NULL;
+SELECT * FROM departamento WHERE id IN (SELECT departamento.id FROM departamento LEFT JOIN profesor ON profesor.id_departamento = departamento.id WHERE profesor.id_profesor IS NULL);
 -- 13   Retorna un llistat amb els professors que no imparteixen cap assignatura.
-SELECT * FROM persona RIGHT JOIN profesor ON persona.id = profesor.id_profesor LEFT JOIN asignatura ON persona.id = asignatura.id_profesor WHERE asignatura.id_profesor IS NULL;
+SELECT * FROM persona WHERE id IN (SELECT persona.id FROM persona RIGHT JOIN profesor ON persona.id = profesor.id_profesor LEFT JOIN asignatura ON persona.id = asignatura.id_profesor WHERE asignatura.id_profesor IS NULL);
 -- 14   Retorna un llistat amb les assignatures que no tenen un professor assignat.
-SELECT * FROM asignatura LEFT JOIN profesor ON asignatura.id_profesor != profesor.id_profesor WHERE asignatura.id_profesor IS NULL;
+SELECT * FROM asignatura WHERE id IN (SELECT asignatura.id FROM asignatura LEFT JOIN profesor ON asignatura.id_profesor != profesor.id_profesor WHERE asignatura.id_profesor IS NULL);
 -- 15   Retorna un llistat amb tots els departaments que no han impartit assignatures en cap curs escolar.
 SELECT DISTINCT(departamento.nombre) as departamento FROM asignatura RIGHT JOIN alumno_se_matricula_asignatura ON asignatura.id = id_asignatura RIGHT JOIN profesor ON profesor.id_profesor = asignatura.id_profesor RIGHT JOIN departamento ON departamento.id = profesor.id_departamento WHERE asignatura.id IS NULL;
 -- 16   Retorna el nombre total d'alumnes que hi ha.
@@ -51,4 +51,4 @@ SELECT persona.id, persona.nombre, persona.apellido1, persona.apellido2, COUNT(a
 -- 25   Retorna totes les dades de l'alumne més jove.
 SELECT * FROM persona WHERE tipo = 'alumno' ORDER BY fecha_nacimiento DESC LIMIT 1;
 -- 26   Retorna un llistat amb els professors que tenen un departament associat i que no imparteixen cap assignatura.
-SELECT * FROM persona INNER JOIN profesor ON persona.id = profesor.id_profesor WHERE profesor.id_departamento IS NOT NULL AND NOT EXISTS (SELECT id_profesor FROM asignatura WHERE id_profesor = profesor.id_profesor);
+SELECT * FROM persona WHERE id IN (SELECT persona.id FROM persona INNER JOIN profesor ON persona.id = profesor.id_profesor WHERE profesor.id_departamento IS NOT NULL AND NOT EXISTS (SELECT id_profesor FROM asignatura WHERE id_profesor = profesor.id_profesor));
